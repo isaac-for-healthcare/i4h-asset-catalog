934cbf7e23f2dd23b205234732eb8039c2f9f950
Mingxin Zheng <mingxinz@nvidia.com>
2025-03-10 05:35:15 +0000

    fix
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- i4h_asset_helper/tests/test_assets.py
+++ i4h_asset_helper/tests/test_assets.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-from unittest.mock import patch
 
 from i4h_asset_helper import get_i4h_asset_path, get_i4h_local_asset_path
 from i4h_asset_helper.assets import _I4H_ASSET_ROOT, _get_sha256_hash
@@ -9,7 +8,8 @@ from i4h_asset_helper.assets import _I4H_ASSET_ROOT, _get_sha256_hash
 def test_get_i4h_asset_path_valid_version():
     # Test with valid version
     result = get_i4h_asset_path(version="0.1")
-    expected_path = f"{_I4H_ASSET_ROOT['nucleus']}/Library/IsaacHealthcare/0.1/i4h-assets-v0.1-{_get_sha256_hash()['0.1']}.zip"
+    hash = _get_sha256_hash()["0.1"]
+    expected_path = f"{_I4H_ASSET_ROOT['nucleus']}/Library/IsaacHealthcare/0.1/i4h-assets-v0.1-{hash}.zip"
     assert result == expected_path
 
 def test_get_i4h_asset_path_invalid_version():
